34dc1dcdfa0876ee8423868064785c842be8c319
muneer-ahmed-khan <muneerkhan31886@gmail.com>
2020-03-11 18:01:04 +0500

    add blog detail page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 
 from .models import Blog
 
@@ -6,3 +6,8 @@ from .models import Blog
 def all_blogs(requests):
     all_blog = Blog.objects.all()
     return render(requests, 'blog/all_blog.html', {'all_blog': all_blog})
+
+
+def details(requests, blog_id):
+    blog = get_list_or_404(Blog, pk=blog_id)
+    return render(requests, 'blog/details.html', {'blog': blog[0]})
