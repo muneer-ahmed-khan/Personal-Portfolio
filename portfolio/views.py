de721b33ae29ed9db2035ff1836e44a6fbdfcad2
muneer-ahmed-khan <muneerkhan31886@gmail.com>
2020-03-11 16:01:50 +0500

    Make simple Project templates

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render
+
+from .models import Project
+
+
+def home(requests):
+    projects = Project.objects.all()
+    return render(requests, 'portfolio/home.html', {'projects': projects})
